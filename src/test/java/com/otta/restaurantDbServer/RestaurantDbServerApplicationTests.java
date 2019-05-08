ab8add95dd8007c17adceb2b3e0b1c55e9eb1e0e
Guilherme <guilhermeotta@gmail.com>
2019-05-08 18:13:53 -0300

    [restaurant] Commit inicial.

---
+++ src/test/java/com/otta/restaurantDbServer/RestaurantDbServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.otta.restaurantDbServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantDbServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
