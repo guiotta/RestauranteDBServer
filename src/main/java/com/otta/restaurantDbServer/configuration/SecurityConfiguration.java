08521c9767c8a35e0442441bdc743389f049a201
Guilherme <guilhermeotta@gmail.com>
2019-05-08 23:03:09 -0300

    [restaurant] Ajuste na configuração do Security.
       - Permite conectar com Postman direto no serviço.

--- src/main/java/com/otta/restaurantDbServer/configuration/SecurityConfiguration.java
+++ src/main/java/com/otta/restaurantDbServer/configuration/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -43,7 +44,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http.
-	            authorizeRequests()
+	            httpBasic().and()
+	            .authorizeRequests()
 	            .antMatchers("/").permitAll()
 	            .antMatchers("/login").permitAll()
 	            .antMatchers("/registration").permitAll()
